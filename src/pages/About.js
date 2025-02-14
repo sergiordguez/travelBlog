6c443470f59f8a66dc4ad3310a9bcf65ed0f3987
sergiorodriguezgarcia <132291274+sergiordguez@users.noreply.github.com>
2025-02-14 16:33:03 +0100

    Probando nuevo color de fondo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+import photo2 from "../assets/photos/about/IMG_3985.JPG";
 import photo3 from "../assets/photos/about/IMG_4030.jpg";
-import photo4 from "../assets/photos/about/IMG_4041.JPG";
 import photo5 from "../assets/photos/about/IMG_4116.JPG";
 import photo6 from "../assets/photos/about/IMG_6948.JPG";
 import photo7 from "../assets/photos/about/IMG_7749.jpg";
 
 import "../styles/AboutUs.css";
 
-const images = [photo3, photo4, photo5, photo6, photo7].sort(() => Math.random() - 0.5);
+const images = [photo2, photo3, photo5, photo6, photo7].sort(() => Math.random() - 0.5);
 
 const AboutUs = () => {
     const [index, setIndex] = useState(0);
