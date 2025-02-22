66855c8e54091df16b90d5c9cf6e13102e0ee3d1
sergiorodriguezgarcia <132291274+sergiordguez@users.noreply.github.com>
2025-02-22 13:03:11 +0100

    Añadir links en sobre nosotros

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,23 +16,31 @@ const AboutUs = () => {
     useEffect(() => {
         const interval = setInterval(() => {
         setIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000); // Cambia cada 10 segundos
+        }, 10000); 
 
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div className="about-us-container">
-        <h1 className="about-title">Sobre Nosotros</h1>
-        <p className="about-text">
-            Somos Paula y Sergio y nos encanta viajar. En este blog compartimos nuestras experiencias para que puedas disfrutar de tus viajes tanto como nosotros.
-        </p>
-        <p className="about-text">
-            Puedes seguirnos en nuestras cuentas de instagram y tiktok para ver más fotos y vídeos de nuestros viajes.
-        </p>
-        <div className="about-gallery" style={{ width: "40%", margin: "0 auto" }}>
-            <img src={images[index]} alt="Viaje" className="about-photo" style={{ width: "100%", height: "auto", borderRadius: "10px" }} />
-        </div>
+            <h1 className="about-title">Sobre Nosotros</h1>
+            <p className="about-text">
+                Somos Paula y Sergio y nos encanta viajar. En este blog compartimos nuestras experiencias para que puedas disfrutar de tus viajes tanto como nosotros.
+            </p>
+            <p className="about-text">
+                Puedes seguirnos en nuestras cuentas de instagram y tiktok para ver más fotos y vídeos de nuestros viajes.
+            </p>
+            <div className="about-gallery" style={{ width: "40%", margin: "0 auto" }}>
+                <img src={images[index]} alt="Viaje" className="about-photo" style={{ width: "100%", height: "auto", borderRadius: "10px" }} />
+            </div>
+            <div className="social-links">
+                <a href="https://www.tiktok.com/@pauuuli22" target="_blank" rel="noopener noreferrer">
+                TikTok
+                </a>
+                <a href="https://www.instagram.com/sergiomoreda7/?hl=es" target="_blank" rel="noopener noreferrer">
+                Instagram
+                </a>
+            </div>
         </div>
     );
     };
